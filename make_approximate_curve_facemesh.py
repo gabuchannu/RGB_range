9cedbc18af2e0b6012db6911d41baf2fefee9bc2
gabuchannu <m.shimizu.0320@gmail.com>
2022-07-15 15:03:51 +0900

    コード改変

--- make_approximate_curve_facemesh.py
+++ make_approximate_curve_facemesh.py
@@ -1,3 +1,4 @@
+from cmath import pi
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot
@@ -8,7 +9,7 @@ from sklearn import linear_model
 #----------------------
 #フレーム毎に処理を行う関数
 #----------------------
-def frame_process(img, frame_count):
+def frame_process(img, frame_count, interval, step):
     
     facemesh = face_mesh_matsumoto.Facemesh(0.7, 0.5) #facemeshを呼びだす
     results = facemesh.run(img) #入ってきたフレーム画像に対してfacemeshを行う
@@ -19,7 +20,7 @@ def frame_process(img, frame_count):
 
     else: #顔検出出来ていたら
         #書き換えポイント
-        file_name = ".//csv_result//hino//frame" + str(frame_count) + ".csv"
+        file_name = "..//result//csv_result//hino//frame" + str(frame_count) + ".csv"
         f = open(file_name, "a") #追記モードでファイルを開く
         f.write("R Value" + "," + "G Value" + "," + "B Value" + "," + "R+G+B Value" + "," + "R-B Value" + "\n") #ヘッダー作成
         
@@ -41,17 +42,22 @@ def frame_process(img, frame_count):
 
                 f.write(str(R_value) + "," + str(G_value) + "," + str(B_value) + "," + str(RGB_Value) + "," + str(RB_Value) + "\n")
 
-    f.close()
-    make_cut_data(frame_count) #現在見ているフレーム数を持って値を切り取りする関数に渡す
+        f.close()
+
+    make_cut_data(frame_count, interval, step) #現在見ているフレーム数を持って値を切り取りする関数に渡す
 
 
-def make_cut_data(frame_count):
+def make_cut_data(frame_count, interval, step):
 
     #先の関数で作った1フレームの各成分値のデータ
-    file_name = ".//csv_result//hino//frame" + str(frame_count) + ".csv"
+    file_name = "..//result//csv_result//hino//frame" + str(frame_count) + ".csv"
+    #if ((765 - interval) % step) == 0:
+    file_number = ((765 - interval) // (step - 1)) + 2
+    #else:
+    #    file_number = ((765 - interval) // (step - 1)) + 1
     
     data = [] #全てのデータを入れる配列を作成
-    for i in range(96): #CSVの数だけ配列を作る
+    for i in range(file_number): #CSVの数だけ配列を作る
         data.append([]) #2次元配列にする
 
     # データの読み込み
@@ -60,34 +66,41 @@ def make_cut_data(frame_count):
 
     total_data_number = len(df["R+G+B Value"]) #1フレーム内の全ピクセル数を求める
 
-    for i in range(total_data_number): #全データを順繰りに見ていく
-        #i行目の3列目(R+G+B成分値)
-        csv_number = df.iat[i, 3] // 8 #商の値を取ってくる
-        pickup_data = [] #見ているデータを入れる配列
-        for j in range(5): #R,G,B,R+G+B,R-Bの5項目を見てappendする
-            pickup_data.append(df.iat[i, j])
-        data[csv_number].append(pickup_data) #商の数番目のCSVファイルに書き込むデータにする
+    for i in range(file_number):
+        for j in range(total_data_number): #全データを順繰りに見ていく
+            if i == 0:
+                if (df.iat[j, 3] // (interval + i * (step - 1))) == 0: #j行目の3列目(R+G+B成分値)
+                    pickup_data = [] #見ているデータを入れる配列
+                    for k in range(5):
+                        pickup_data.append(df.iat[j, k]) #R,G,B,R+G+B,R-B全部のデータを入れる
+                    data[i].append(pickup_data) #追加する
+            else:
+                if ((df.iat[j, 3] // (interval + i * (step - 1))) == 0) & (df.iat[j, 3] >= i * (step - 1)): #j行目の3列目(R+G+B成分値)
+                    pickup_data = [] #見ているデータを入れる配列
+                    for k in range(5):
+                        pickup_data.append(df.iat[j, k]) #R,G,B,R+G+B,R-B全部のデータを入れる
+                    data[i].append(pickup_data) #追加する
 
     count = 0
 
-    for i in range(96): #CSVに書き込みしていく
+    for i in range(file_number): #CSVに書き込みしていく
         new_df =  pd.DataFrame(data[i], columns=["R Value", "G Value", "B Value", "R+G+B Value", "R-B Value"]) #新しいファイルを作成する
-        new_df_name = ".//csv_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count+7) + ".csv" #新しいCSVを作成する
+        new_df_name = "..//result//csv_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count + interval - 1) + ".csv" #新しいCSVを作成する
         new_df.to_csv(new_df_name) #CSVに切ったデータを書き込む
-        count = count + 8
+        count = count + step - 1
     
-    make_graph(frame_count, total_data_number)
+    make_graph(frame_count, total_data_number, interval, step, file_number)
 
 
 
 #-------------------
 #グラフを描く関数
 #-------------------
-def make_graph(frame_count, total_data_number):
+def make_graph(frame_count, total_data_number, interval, step, file_number):
 
     count = 0
-    for i in range(96):
-        look_df_name = ".//csv_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count+7) + ".csv" #開くファイルの名前を指定
+    for i in range(file_number):
+        look_df_name = "..//result//csv_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count + interval - 1) + ".csv" #開くファイルの名前を指定
         #look_df = pd.read_csv(look_df_name, encoding="shift_jis")
         look_df = pd.read_csv(look_df_name, encoding="utf-8") #CSVをDFとして開く
 
@@ -95,11 +108,11 @@ def make_graph(frame_count, total_data_number):
 
             fig = pyplot.figure()
             ax = fig.add_subplot(1, 1, 1)
-            fig_name = ".//figure_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count+7) + ".png" #ファイルの名前
+            fig_name = "..//result//figure_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count + interval - 1) + ".png" #ファイルの名前
 
             pyplot.xlim(0, 765) #R+G+Bの最大値まで
             pyplot.ylim(0, 255) #R-Bの最大値まで
-            title = "Scatter of R+G+B" + str(count) + "~" + str(count+7) + "(frame" + str(frame_count) + ")"
+            title = "Scatter of R+G+B" + str(count) + "~" + str(count + interval - 1) + "(frame" + str(frame_count) + ")"
             pyplot.title(title)
             pyplot.xlabel("R+G+B Value")
             pyplot.ylabel("R-B Value")
@@ -122,25 +135,26 @@ def make_graph(frame_count, total_data_number):
             approximate_formula = "y = " + str(f'{b[0]:.3f}') + " + " + str(f'{a[0][0]:.3f}') + " * x" #近似直線の式
 
             xinterval = 0.1 #近似直線を表示するためのxダミーデータを作る
-            x = np.arange(count-30, count+37, xinterval)
+            x = np.arange(count-80, count+80, xinterval)
             y = a[0][0] * x + b[0]
 
             density = len(look_df["R+G+B Value"]) / total_data_number * 100 #密度を%で表示する
 
-            if r2 == "NAN":
-                text = "r^2:" + str(r2)
-            else:
-                text = "r^2:" + str(format(r2, '.2f'))
+            # if r2 == "NAN":
+            #     text = "r^2:" + str(r2)
+            # else:
+            #     text = "r^2:" + str(format(r2, '.2f'))
 
             pyplot.scatter(look_df["R+G+B Value"], look_df["R-B Value"]) #散布図を描く
             pyplot.plot(x, y, color="coral") #近似直線を描く
-            pyplot.text(20, 230, text)
+            # pyplot.text(20, 230, text)
 
             pyplot.legend(["density:" + f'{density:.3f}' + "%", "regressionline:" + approximate_formula])
 
             fig.savefig(fig_name)
+            pyplot.close(fig)
         
-        count = count + 8
+        count = count + step - 1
 
 
 #-------------
@@ -148,12 +162,15 @@ def make_graph(frame_count, total_data_number):
 #-------------
 if __name__=="__main__":
 
+    interval = int(input("範囲の幅を数値入力してください："))
+    step = int(input("ずらしていく幅を数値入力してください："))
+
     count = 1 #全フレーム(1秒に30枚)に対してランドマークはしないのでカウントフラグを使う
     frame_count = 1 #CSVファイルの時間を書き込むためのカウント
 
     #ビデオを読み込みする
     #書き換えポイント1
-    cap = cv2.VideoCapture(".//data_movie//hino_yoko.mp4")
+    cap = cv2.VideoCapture("..//data_movie//hino_yoko.mp4")
 
     while True: #動画が終わるまで続ける
         ret, img = cap.read()
@@ -163,7 +180,7 @@ if __name__=="__main__":
 
         if count == 30: #1秒経過していたら
             count = 1 #カウンターを初期化
-            frame_process(img, frame_count) #取り出したimgに対してランドマーク
+            frame_process(img, frame_count, interval, step) #取り出したimgに対してランドマーク
             frame_count = frame_count + 1 #1増やす
 
         else: #countが30未満だったら
--- make_approximate_curve_facemesh_step8_32_total.py
+++ make_approximate_curve_facemesh_step8_32_total.py
@@ -1,3 +1,4 @@
+from cmath import pi
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot
@@ -51,7 +52,7 @@ def make_cut_data(frame_count):
     file_name = ".//csv_result//hino//frame" + str(frame_count) + ".csv"
     
     data = [] #全てのデータを入れる配列を作成
-    for i in range(96): #CSVの数だけ配列を作る
+    for i in range(106): #CSVの数だけ配列を作る
         data.append([]) #2次元配列にする
 
     # データの読み込み
@@ -60,62 +61,83 @@ def make_cut_data(frame_count):
 
     total_data_number = len(df["R+G+B Value"]) #1フレーム内の全ピクセル数を求める
 
-    for i in range(total_data_number): #全データを順繰りに見ていく
-        #i行目の3列目(R+G+B成分値)
-        csv_number = df.iat[i, 3] // 8 #商の値を取ってくる
-        pickup_data = [] #見ているデータを入れる配列
-        for j in range(5): #R,G,B,R+G+B,R-Bの5項目を見てappendする
-            pickup_data.append(df.iat[i, j])
-        data[csv_number].append(pickup_data) #商の数番目のCSVファイルに書き込むデータにする
+    for i in range(106):
+        for j in range(total_data_number): #全データを順繰りに見ていく
+            #i行目の3列目(R+G+B成分値)
+            if i == 0:
+                if (df.iat[j, 3] // (32 + i * 7)) == 0:
+                    pickup_data = [] #見ているデータを入れる配列
+                    for k in range(5):
+                        pickup_data.append(df.iat[j, k])
+                    data[i].append(pickup_data)
+            else:
+                if ((df.iat[j, 3] // (32 + i * 7)) == 0) & (df.iat[j, 3] >= i * 7):
+                    pickup_data = [] #見ているデータを入れる配列
+                    for k in range(5):
+                        pickup_data.append(df.iat[j, k])
+                    data[i].append(pickup_data)
 
     count = 0
 
-    for i in range(96): #CSVに書き込みしていく
+    for i in range(106): #CSVに書き込みしていく
         new_df =  pd.DataFrame(data[i], columns=["R Value", "G Value", "B Value", "R+G+B Value", "R-B Value"]) #新しいファイルを作成する
-        new_df_name = ".//csv_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count+7) + ".csv" #新しいCSVを作成する
+        new_df_name = ".//csv_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count+31) + ".csv" #新しいCSVを作成する
         new_df.to_csv(new_df_name) #CSVに切ったデータを書き込む
-        count = count + 8
-    
-    make_graph(frame_count, total_data_number)
+        count = count + 7
 
 
 
 #-------------------
 #グラフを描く関数
 #-------------------
-def make_graph(frame_count, total_data_number):
+def make_graph(frame_count):
 
     count = 0
-    for i in range(96):
-        look_df_name = ".//csv_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count+7) + ".csv" #開くファイルの名前を指定
-        #look_df = pd.read_csv(look_df_name, encoding="shift_jis")
-        look_df = pd.read_csv(look_df_name, encoding="utf-8") #CSVをDFとして開く
+    color = ["red", "coral", "gold", "yellowgreen", "forestgreen", "deepskyblue", "mediumblue", "darkviolet", "hotpink", "sienna", "grey", "black"]
 
-        if len(look_df["R+G+B Value"]) != 0: #要素が存在するのであれば
+    for i in range(106):
+        total_data_x = [] #全フレームのi番目の範囲のデータを入れる
+        total_data_y = []
+        total_data_number = 0
 
-            fig = pyplot.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            fig_name = ".//figure_result//hino//frame" + str(frame_count) + "_" + str(count) + str(count+7) + ".png" #ファイルの名前
+        #グラフ作成の書式
+        fig = pyplot.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        fig_name = ".//figure_result//hino//frame_total_" + str(count) + str(count+31) + ".png" #ファイルの名前
+        pyplot.xlim(0, 765) #R+G+Bの最大値まで
+        pyplot.ylim(0, 255) #R-Bの最大値まで
+        title = "Scatter of R+G+B" + str(count) + "~" + str(count+31) + "(total_frame)"
+        pyplot.title(title)
+        pyplot.xlabel("R+G+B Value")
+        pyplot.ylabel("R-B Value")
 
-            pyplot.xlim(0, 765) #R+G+Bの最大値まで
-            pyplot.ylim(0, 255) #R-Bの最大値まで
-            title = "Scatter of R+G+B" + str(count) + "~" + str(count+7) + "(frame" + str(frame_count) + ")"
-            pyplot.title(title)
-            pyplot.xlabel("R+G+B Value")
-            pyplot.ylabel("R-B Value")
+        for j in range(frame_count-1):
+            look_df_name = ".//csv_result//hino//frame" + str(j+1) + "_" + str(count) + str(count+31) + ".csv" #開くファイルの名前を指定
+            look_df = pd.read_csv(look_df_name, encoding="utf-8") #CSVをDFとして開く
 
-            clf = linear_model.LinearRegression()
+            if len(look_df["R+G+B Value"]) != 0: #要素が存在するのであれば
+                for k in range(len(look_df["R+G+B Value"])):
+                    total_data_x.append(look_df.iat[k,3])
+                    total_data_y.append(look_df.iat[k,4])
+                total_data_number = total_data_number + len(look_df["R-B Value"])
+                pyplot.scatter(look_df["R+G+B Value"], look_df["R-B Value"], color = color[j]) #散布図を描く
 
-            x_data = look_df[["R+G+B Value"]] #xのデータとしてR+G+B成分値
-            y_data = look_df[["R-B Value"]] #yのデータとしてR-B成分値
+        if total_data_number != 0:
+            x_col = ["R+G+B Value"]
+            y_col = ["R-B Value"]
+
+            x_data_df = pd.DataFrame(data=total_data_x, columns=x_col)
+            y_data_df = pd.DataFrame(data=total_data_y, columns=y_col)
+            
+            clf = linear_model.LinearRegression()     
 
             #近似直線を作るための傾きと切片を出す
-            clf.fit(x_data, y_data)
+            clf.fit(x_data_df, y_data_df)
             a = clf.coef_
             b = clf.intercept_
 
-            if len(x_data) >= 2:
-                r2 = clf.score(x_data, y_data)
+            if len(x_data_df) >= 2:
+                r2 = clf.score(x_data_df, y_data_df)
             else:
                 r2 = "NAN"
 
@@ -123,7 +145,7 @@ def make_graph(frame_count, total_data_number):
 
             xinterval = 0.1 #近似直線を表示するためのxダミーデータを作る
             x = np.arange(count-30, count+37, xinterval)
-            y = a[0][0] * x + b[0]
+            y = a[0] * x + b
 
             density = len(look_df["R+G+B Value"]) / total_data_number * 100 #密度を%で表示する
 
@@ -132,15 +154,15 @@ def make_graph(frame_count, total_data_number):
             else:
                 text = "r^2:" + str(format(r2, '.2f'))
 
-            pyplot.scatter(look_df["R+G+B Value"], look_df["R-B Value"]) #散布図を描く
             pyplot.plot(x, y, color="coral") #近似直線を描く
             pyplot.text(20, 230, text)
 
             pyplot.legend(["density:" + f'{density:.3f}' + "%", "regressionline:" + approximate_formula])
 
+            pyplot.close(fig)
             fig.savefig(fig_name)
         
-        count = count + 8
+            count = count + 7       
 
 
 #-------------
@@ -168,5 +190,8 @@ if __name__=="__main__":
 
         else: #countが30未満だったら
             count = count + 1 #countを増やす
+
+    max_frame_count = frame_count
+    make_graph(frame_count)
 
     cap.release()
